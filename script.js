d8cdb383b28953a3313ab2e042358637eb43232c
Bencodes1 <256.ben@gmail.com>
2021-02-01 08:39:44 -0500

    working through step 5

--- script.js
+++ script.js
@@ -2,6 +2,39 @@ console.log('Add validation!');
 
 const form = document.querySelector('#parking-form')
 
+
 window.addEventListener('submit', event => {
     event.preventDefault()
 })
+
+let validcheck
+function validate(){
+    console.log('Validate check');
+    validcheck = true;
+    confirmValid();
+}
+
+function confirmValid(){
+    console.log('confirmValid check')
+    if(validcheck && form.checkValidity()){
+        const validinput1 = document.querySelector('#total')
+        validinput1.innerText = totalExpense()
+    }
+    else{
+        console.log("Debug loc 1")
+    }
+}
+
+let price = 5
+
+function totalExpense() {
+    console.log('totalExpense check')
+    const numdays = document.querySelector('#days').value
+    console.log('total days: ', numdays)
+ 
+    let startdate = document.querySelector('#start-date').value
+    console.log("Start Date:  ", startdate)
+
+    let fullPrice = numdays * price;
+    return "Total cost: $",fullPrice;
+}
